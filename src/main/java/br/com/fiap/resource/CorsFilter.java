301c465f57deb9f5d773441778f51718cd33ca47
felipeflosii <luizfelipeflosi@gmail.com>
2025-11-21 19:26:11 -0300

    chore: Corrigindo problema de CORS no arquivo CorsFilter

--- src/main/java/br/com/fiap/resource/CorsFilter.java
+++ src/main/java/br/com/fiap/resource/CorsFilter.java
@@ -14,10 +14,13 @@ public class CorsFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
 
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        String frontendUrl = "http://localhost:5173";
+
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", frontendUrl);
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 
+        responseContext.getHeaders().add("Access-Control-Max-Age", "3600");
     }
 }
